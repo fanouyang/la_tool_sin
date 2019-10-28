3a779c3d28af565120a697c5a11dcda46be60292
Fan Ouyang <fanouyang@zju.edu.cn>
2019-10-28 16:53:00 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,31 @@
+
+library(shiny)
+library(visNetwork)
+shinyUI(fluidPage(
+  
+  titlePanel("sis_demo"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      selectInput("week", "Choose a week:", 
+                  c("Week01"=1,"Week02"=2,"Week03"=3),
+                  helpText("This is a tool demo"
+                  ),
+                  width="100%"
+      )
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("student-student network", visNetworkOutput("stos",height = '650px')), 
+        tabPanel("student-term network", visNetworkOutput("stot",height = '650px')),
+        tabPanel("term-term network", visNetworkOutput("ttot",height = '650px')),
+        tabPanel("More info", htmlOutput("info"))
+        
+        
+      )
+    )
+  )
+))
